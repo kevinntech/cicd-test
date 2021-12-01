e8aa7993a2a55a1b2e01397054605279e721da95
kevinntech <kevinntech@nate.com>
2021-12-02 03:22:06 +0900

    finish cicd.html

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,10 +4,11 @@ chmod +x deploy.sh
 
 echo "> 현재 구동중인 Jar 파일 종료"
 
-pkill -9 'java -jar'
+pkill -f 'java -jar'
+sleep 5
 
 echo "> Jar 파일 실행"
 
-java -jar *.jar &
+nohup java -jar *.jar &
 
 echo "> Jar 파일 실행 완료"
